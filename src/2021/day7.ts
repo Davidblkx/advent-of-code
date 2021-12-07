aee9ab904e3685fc4c8886709b16a1ab29c57df6
David Pires <developer@davidpires.pt>
2021-12-07 07:17:05 +0000

    2021 day 7

--- src/2021/day7.ts
+++ src/2021/day7.ts
@@ -1,5 +1,44 @@
 
 // Solution for day 7 of Advent of Code 2021
 export function execute(_input: string): Promise<number[]> {
-  return Promise.resolve([0, 0]);
+  const input = _input.split(",").map(Number);
+
+  return Promise.resolve([
+    calcShortestDistance(input, false),
+    calcShortestDistance(input, true),
+  ]);
+}
+
+function findDistance(array: number[], target: number): number {
+  let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total += Math.abs(array[i] - target);
+  }
+  return total;
+}
+
+function findGradualDistance(array: number[], target: number): number {
+  let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    const steps = Math.abs(array[i] - target);
+    for (let s = 1; s <= steps; s++) {
+      total += s;
+    }
+  }
+  return total;
+}
+
+function calcShortestDistance(array: number[], gradual: boolean): number {
+  const min = Math.min(...array);
+  const max = Math.max(...array);
+  let result = Infinity;
+
+  for (let d = min; d <= max; d++) {
+    const distance = gradual ? findGradualDistance(array, d) : findDistance(array, d);
+    if (distance < result) {
+      result = distance;
+    }
+  }
+
+  return result;
 }
